82ebc3b20aa65015d87a9ffd5a31f2ef4ee045db
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-11-20 01:37:56 +0700

    add utils

---
+++ src/utils/FormatDate.js
@@ -0,0 +1,8 @@
+export default function formatDate(mongoDate) {
+  const date = new Date(mongoDate);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
